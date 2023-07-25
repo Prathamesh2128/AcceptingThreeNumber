4a4508cd20d3559f5ea346adf4755dc791a9b885
Harshad041998 <harshadlawar1@gmail.com>
2023-07-25 08:19:02 +0530

    Added java code for Accept threee number and find largest number

---
+++ AccepetThreeNumer/src/com/app/Solution.java
@@ -0,0 +1,21 @@
+package com.app;
+
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+
+        System.out.print("Enter the third number: ");
+        double num3 = scanner.nextDouble();
+
+        double largest = Math.max(num1, Math.max(num2, num3));
+        System.out.println("The largest number is: " + largest);
+	}
+}
